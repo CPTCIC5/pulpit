948d901a9286d3d879025786de2cc305b07ad591
CPTCIC5 <aryanjainak@gmail.com>
2025-03-26 00:34:10 +0530

    XYZ

--- api/settings.py
+++ api/settings.py
@@ -76,8 +76,12 @@ CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'  # Ensure this for cross-site requests
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_HTTPONLY = False
-CSRF_TRUSTED_ORIGINS = ['*']
-CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = [
+    'https://podiam.app',
+    'https://35.183.155.122',
+    'http://localhost:3000',
+    'https://frontend-pulpit.vercel.app'
+]
 ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
