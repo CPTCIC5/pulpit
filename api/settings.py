d0c6f4d884503b839af1eb792449c500802133d3
CPTCIC5 <aryanjainak@gmail.com>
2025-03-25 23:42:10 +0530

    XYZ

--- api/settings.py
+++ api/settings.py
@@ -225,7 +225,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_ALLOWED_ORIGINS = CORS_ALLOWED_ORIGINS
 #CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-APPEND_SLASH = True
+APPEND_SLASH = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
